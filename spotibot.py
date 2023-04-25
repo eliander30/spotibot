7d085950cec1dc23761a484b4c30e21cccce4294
Eliander Perez <37315601+eliander30@users.noreply.github.com>
2023-04-24 20:27:18 -0400

    Add files via upload

---
+++ spotibot.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver import ActionChains
+import random
+
+EMAIL = "testerprome123@proton.me"
+PASSWORD = "tutetatota123"
+PLAYLIST = "https://spotify.app.link/b2FDqju8ezb?_p=c51629ca911c65f0e1038bfb"
+
+driver = webdriver.Chrome()
+	
+driver.maximize_window()
+	
+driver.get("https://accounts.spotify.com/en/login")
+	
+time.sleep(10)
+	
+user = driver.find_element(By.ID, "login-username")
+password = driver.find_element(By.ID, "login-password")
+	
+user.send_keys(EMAIL)
+password.send_keys(PASSWORD)
+	
+login = driver.find_element(By.CLASS_NAME, "Button-sc-qlcn5g-0.otMlU")
+
+time.sleep(5)
+	
+login.click()
+	
+time.sleep(5)
+	
+print("[*] Login Succesfully")
+	
+driver.get(PLAYLIST)
+	
+time.sleep(30)
+
+while True:
+
+	checkPlay = random.randint(1100,1800)	
+	
+#	firstSong = driver.find_element(By.CLASS_NAME, "Type__TypeElement-sc-goli3j-0.gYqKuy.t_yrXoUO3qGsJS4Y6iXX.standalone-ellipsis-one-line")
+	
+#	actionChains = ActionChains(driver)
+	
+#	actionChains.double_click(firstSong).perform()
+	
+	
+	play = driver.find_element(By.CLASS_NAME, "Button-sc-qlcn5g-0.jqMzOG")
+	
+	play.click()
+	
+	
+	streamTime = driver.find_element(By.CLASS_NAME, "poz9gZKE7xqFwgk231J4")
+	
+	hours = streamTime.text.split(" ")
+	
+	print("[*] Playlist time Lenght: "+ hours[1] + "hours")
+	
+	seconds = int(hours[1]) * 3600
+	
+	titlePlaylist = driver.find_element(By.CLASS_NAME, "Type__TypeElement-sc-goli3j-0.bZdlXz")
+	
+	print("[*] Playing now >> " + titlePlaylist.text)
+	
+	time.sleep(checkPlay)
+	
+	driver.refresh()
